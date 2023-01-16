675e16125c90aaf49099a5da43911416701e422d
Vesa Åkerman <vesaakerman@gmail.com>
2023-01-16 14:04:29 +0100

    Deployed 2d4904a with MkDocs version: 1.4.2

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,4 +1,10 @@
 function print_document() {
+const doc = new jsPDF();
+
+    window.print();
+}
+
+function print_document2() {
     document.getElementById("download").style.display = "none"
     window.print();
     document.getElementById("download").style.display = "block"
