0b18bf9cb0d697b95148cdb402a419a0d2eda858
Vesa Åkerman <vesaakerman@gmail.com>
2024-09-03 16:24:09 +0200

    Added contributors_footnote to metadata and display in a pop up window

--- build.py
+++ build.py
@@ -42,6 +42,8 @@ HEADER = '<html><body><img id="banner" src="../../images/banners/banner.png" alt
 DOWNLOAD = '<div><input id="download" title="Download/print the document" type="image" onclick="print_document()" src="../../images/icons/download3.png" alt="download" /></div>'
 SHEBANQ = '<div><a id="shebanq" title="Word in SHEBANQ" href="https://shebanq.ancient-data.org/hebrew/word?id=replace" target="_blank"><img src="../../images/icons/shebanq.png" alt="shebanq"></a></div>'
 UBS = '<div><a id="ubs" title="Word in Semantic Dictionary of Biblical Hebrew" href="https://semanticdictionary.org/semdic.php?databaseType=SDBH&language=en&lemma=replace&startPage=1" target="_blank"><img src="../../images/icons/ubs.png" alt="ubs"></a></div>'
+CONTRIBUTORS_FOOTNOTE = '<p id="contributors_footnote">{footnote}</p>'
+CONTRIBUTORS_FOOTNOTE_REF = '<sup><a href="#footnote" data-toggle="modal" onclick="show_modal({fn})"> *</a></sup>'.format(fn="'contributors_footnote'")
 
 PHOTO_PATH = r"(.*!\[.*])(\(.*/(.*\.(png|PNG|jpg|JPG|jpeg|JPEG|gif|GIF|tiff|TIFF)))(.*)"
 PHOTO_PATH_REPLACEMENT_HOME = r"\1(./images/photos/\3\5"
@@ -366,7 +368,7 @@ def write_words(shebanq_dict, ubs_dict):
     os.mkdir(WORDS_DOCS)
 
     for word in WORDS.glob("*"):
-        word_hebrew, word_english, transcription, title, shebanq_id, first_published, last_update, additional_info = "", "", "", "", "", "", "", ""
+        word_hebrew, word_english, transcription, title, shebanq_id, contributors_footnote, first_published, last_update, additional_info = "", "", "", "", "", "", "", "", ""
         semantic_fields, contributors = [], []
         text, first_dashes, second_dashes = [], False, False
         if word.name == ".DS_Store":
@@ -397,6 +399,8 @@ def write_words(shebanq_dict, ubs_dict):
                     semantic_fields = get_values(line)
                 elif line.startswith("contributors:"):
                     contributors = get_values(line)
+                elif line.startswith("contributors_footnote:"):
+                    contributors_footnote = get_value(line)
                 elif line.startswith("first_published:"):
                     first_published = get_value(line)
                 elif line.startswith("shebanq_id:"):
@@ -437,6 +441,8 @@ def write_words(shebanq_dict, ubs_dict):
                             contributors_text += f"[{capitalize_name(c)}](../contributors/{c}.md)"
                             contributors_citing += capitalize_name(c)
                             first = False
+                        if (contributors_footnote):
+                            contributors_text += CONTRIBUTORS_FOOTNOTE_REF
                         text.append(contributors_text + "<br>\n")
                     if first_published:
                         text.append(f"First published: {first_published}<br>")
@@ -453,6 +459,8 @@ def write_words(shebanq_dict, ubs_dict):
                     if additional_info:
                         text.append("\n" + additional_info)
                     text.append("\n\n")
+            if contributors_footnote:
+                text.append(CONTRIBUTORS_FOOTNOTE.format(footnote=contributors_footnote))
 
         if not second_dashes:
             error(f"Metadata for {filename} incomplete")
