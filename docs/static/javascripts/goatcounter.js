80544e76fcad783ef3d9d5cf527222e2778973d2
Vesa Åkerman <vesaakerman@gmail.com>
2026-01-05 11:48:00 +0100

    added Goatcounter tracking

---
+++ docs/static/javascripts/goatcounter.js
@@ -0,0 +1,7 @@
+(function () {
+  var s = document.createElement("script");
+  s.async = true;
+  s.src = "//gc.zgo.at/count.js";
+  s.setAttribute("data-goatcounter", "https://sahd.goatcounter.com/count");
+  document.head.appendChild(s);
+})();
