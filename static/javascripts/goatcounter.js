6efddb0fd59e20e73fc59abe5bea9ef18946b094
Vesa Åkerman <vesaakerman@gmail.com>
2026-01-05 12:23:43 +0100

    Deployed 80544e76 with MkDocs version: 1.6.0

--- static/javascripts/goatcounter.js
+++ static/javascripts/goatcounter.js
@@ -1,7 +1,7 @@
 (function () {
   var s = document.createElement("script");
   s.async = true;
-  s.src = "//gc.zgo.at/count.js";
+  s.src = "https://gc.zgo.at/count.js";
   s.setAttribute("data-goatcounter", "https://sahd.goatcounter.com/count");
   document.head.appendChild(s);
 })();
